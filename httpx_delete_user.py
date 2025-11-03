aa6f53fd66256190c1248e81608cdecb36af8f03
Ладыгина Елена Владимировна <e.ladygina@geomix.ru>
2025-11-03 14:49:25 +0700

    Практикуемся в работе с HTTPX

---
+++ httpx_delete_user.py
@@ -0,0 +1,34 @@
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+    "email": get_random_email(),
+    "password": "string",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string"
+}
+
+creatr_user_response = httpx.post("http://localhost:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = creatr_user_response.json()
+
+login_payload = {
+    "email": create_user_payload["email"],
+    "password": create_user_payload["password"],
+}
+
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=create_user_payload)
+login_response_data = login_response.json()
+
+get_user_headers = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+
+get_user_response = httpx.get(f"http://localhost:8000/api/v1/users/{create_user_response_data['user']['id']}",
+                              headers=get_user_headers)
+
+get_user_response_data = get_user_response.json()
+print(get_user_response_data)
+
+delete_user_headers = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+
+delete_user_response = httpx.delete(f"http://localhost:8000/api/v1/users/{create_user_response_data['user']['id']}",
+                                    headers=delete_user_headers)
